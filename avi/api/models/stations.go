7155219f5336c619864ea7d8a7fcabf90158749f
LeBronoMars <lebronomars@gmail.com>
2016-05-17 00:28:36 +0800

    added contact no in station model

--- avi/api/models/stations.go
+++ avi/api/models/stations.go
@@ -5,6 +5,7 @@ type Station struct {
 	StationName  string `json:"station_name" form:"station_name" binding:"required"`
 	Address string `json:"address" form:"address" binding:"required"`
 	Status string `json:"status" form:"status" binding:"required"`
+	ContactNo string `json:"contact_no" form:"contact_no" binding:"required"`
 	StationType string `json:"station_type" form:"station_type" binding:"required"`
 	Latitude float32 `json:"latitude" form:"latitude" binding:"required"`
 	Longitude float32 `json:"longitude" form:"longitude" binding:"required"`
